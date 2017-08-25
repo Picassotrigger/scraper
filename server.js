68a099617bf2c3ab32319823ed7f8c089aa17908
PicassoTrigger <david@picassotrigger.com>
2017-08-25 17:04:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function(req, res) {
 
 
 //------------------------   Database configuration with mongoose   ------------------------
-mongoose.connect("mongodb://localhost/scraper");
+mongoose.connect("mongodb://heroku_fn363mbq:i792qrvppk0t8mh3i4tg721pki@ds149382.mlab.com:49382/heroku_fn363mbq");
 var db = mongoose.connection;
 
 
